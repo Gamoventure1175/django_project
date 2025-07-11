b8f432adc9c91d047c92dbb1bd574b6c25be88b4
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-07-12 00:59:51 +0530

    learned model creating and migration, routing and dynamic routing, loading model data into html

--- my_test_project/members/views.py
+++ my_test_project/members/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
+from .models import Members
 
 def members(request: HttpRequest):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    template = loader.get_template('all_members.html')
+    my_members = Members.objects.all()
+    context = {
+        'members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request: HttpRequest, member_id: int):
+    template = loader.get_template('details.html')
+    member = Members.objects.get(id=member_id)
+    context = {
+        'member': member,
+    }
+    return HttpResponse(template.render(context, request))
