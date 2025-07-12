0af279b98747a65d0964041ecf7e07ea1aa89e3e
unknown <Gaurav Abhiman Mahajan>
2025-07-12 17:49:27 +0530

    Changed the model Members to Member so that it will show up as "Members" on admin dashboard instead of "Memberss". Trying to setup a custom management command operation that will add default 4 entries into database table

--- my_test_project/members/views.py
+++ my_test_project/members/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
-from .models import Members
+from .models import Member
 
 def members(request: HttpRequest):
     template = loader.get_template('all_members.html')
-    my_members = Members.objects.all()
+    my_members = Member.objects.all()
     context = {
         'members': my_members,
     }
@@ -12,7 +12,7 @@ def members(request: HttpRequest):
 
 def details(request: HttpRequest, member_id: int):
     template = loader.get_template('details.html')
-    member = Members.objects.get(id=member_id)
+    member = Member.objects.get(id=member_id)
     context = {
         'member': member,
     }
